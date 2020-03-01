10f3c17e3c89ce435598d8166793ef5950238061
Ramazan Sakin <ramazansakin63@gmail.com>
2020-03-01 17:49:38 +0300

    cleaning up controllers and embedded mongo dependency added

--- src/main/java/com/sakinramazan/soa/web/PersonController.java
+++ src/main/java/com/sakinramazan/soa/web/PersonController.java
@@ -1,86 +1,75 @@
 package com.sakinramazan.soa.web;
 
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
-import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
+import com.sakinramazan.soa.document.Person;
+import com.sakinramazan.soa.service.PersonService;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sakinramazan.soa.document.Person;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.wordnik.swagger.annotations.Api;
-import com.wordnik.swagger.annotations.ApiOperation;
-
-import com.sakinramazan.soa.service.PersonService;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
+import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
 
 @Api(value = "person")
 @RestController
 @RequestMapping("/person")
 public class PersonController {
-	
-	@Autowired
-	private PersonService personService;
-	
-	@ApiOperation(value = "Find person by ID" )
-    @ResponseStatus(HttpStatus.OK)
-    @RequestMapping(value = "/{personId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-    public Person get(@PathVariable(value = "personId") String personId){
-        Person person = personService.findById(personId);
-        addHATEOASSupport(person, personId);
-		return person;
-    }
-	
-	@ApiOperation(value = "Find all persons" )
-	@ResponseStatus(HttpStatus.OK)
+
+    @Autowired
+    private PersonService personService;
+
+	// @ApiOperation annotation is used for describing the functionality of the api on swagger
+	@ApiOperation(value = "Find all persons")
 	@RequestMapping(value = "/findAll", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-	public List<Person> findAll(){
+	public List<Person> findAll() {
 		List<Person> persons = personService.findAll();
-		ArrayList<Person> personsReturn = new ArrayList<Person>();
+		ArrayList<Person> personsReturn = new ArrayList<>();
 		for (Person person : persons) {
-			String idPerson = person.getIdPerson().toString();
+			String idPerson = person.getIdPerson();
 			addHATEOASSupport(person, idPerson);
 			personsReturn.add(person);
 		}
 		return personsReturn;
 	}
-	
-	@ApiOperation(value = "Create a new person" )
-	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
-	public Person create(@RequestBody Person person){
-		Person createdPerson = personService.create(person);
-		String idPerson = createdPerson.getIdPerson().toString();
-		addHATEOASSupport(createdPerson, idPerson);
-		return createdPerson;
-	}
-	
-	@ApiOperation(value = "Update an existing person")
-	@ResponseStatus(HttpStatus.OK)
-	@RequestMapping(method = RequestMethod.PUT,consumes = MediaType.APPLICATION_JSON_VALUE)
-	public Person update(@RequestBody Person person){
-		Person updatedPerson = personService.update(person);
-		String idPerson = updatedPerson.getIdPerson().toString();
-		addHATEOASSupport(updatedPerson, idPerson);		
-		return updatedPerson;
-	}
 
-	@ApiOperation(value = "Delete person by ID" )
-	@ResponseStatus(HttpStatus.OK)
+    @ApiOperation(value = "Find person by ID")
+    @RequestMapping(value = "/{personId}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person get(@PathVariable(value = "personId") String personId) {
+        Person person = personService.findById(personId);
+        addHATEOASSupport(person, personId);
+        return person;
+    }
+
+    @ApiOperation(value = "Create a new person")
+    @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public Person create(@RequestBody Person person) {
+        Person createdPerson = personService.create(person);
+        String idPerson = createdPerson.getIdPerson();
+        addHATEOASSupport(createdPerson, idPerson);
+        return createdPerson;
+    }
+
+    @ApiOperation(value = "Update an existing person")
+    @RequestMapping(method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
+    public Person update(@RequestBody Person person) {
+        Person updatedPerson = personService.update(person);
+        String idPerson = updatedPerson.getIdPerson();
+        addHATEOASSupport(updatedPerson, idPerson);
+        return updatedPerson;
+    }
+
+    @ApiOperation(value = "Delete person by ID")
     @RequestMapping(value = "/{personId}", method = RequestMethod.DELETE)
-    public void delete(@PathVariable(value = "personId") String personId){
+    public void delete(@PathVariable(value = "personId") String personId) {
         personService.delete(personId);
     }
 
-	private void addHATEOASSupport(Person person, String idPerson) {
-		person.add(linkTo(methodOn(PersonController.class).get(idPerson)).withSelfRel());
-	}
+    private void addHATEOASSupport(Person person, String idPerson) {
+        person.add(linkTo(methodOn(PersonController.class).get(idPerson)).withSelfRel());
+    }
 }
